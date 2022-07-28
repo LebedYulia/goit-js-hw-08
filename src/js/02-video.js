c30b7b691437520b45ca3006d0a6b9fcb0d15531
Lebed Yulia <YuLebed@ukr.net>
2022-07-28 18:59:12 +0300

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+player.on('timeupdate', throttle(writeCurrentTime, 1000));
 
+
+function writeCurrentTime({seconds}) {    
+   localStorage.setItem("videoplayer-current-time", seconds);
+   console.log({seconds})
+}
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(currentTime);
